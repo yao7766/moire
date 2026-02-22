07af2851a04dd9c90114865c9f0696b44f198c78
Penny <141225442+yao7766@users.noreply.github.com>
2026-02-22 21:50:40 +0800

    Update config properties for title, author, and theme

--- moire.config.ts
+++ moire.config.ts
@@ -1,10 +1,10 @@
 export const config = {
-  title: "Moire",
-  author: "Aeris",
-  theme: "classic",
+  title: Verso",
+  author: "Yao",
+  theme: "receipt",
   pageSize: 20,
   order_by: "created",
-  description: "Sync your thoughts from Apple Notes by Shortcuts.",
+  description: "INFP的电子杂志🦋",
   keywords: "memo, thought, sync, apple notes, shortcuts, ios",
   url: "https://moire.blog"
 };
